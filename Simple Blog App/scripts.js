112f7b13687789e4e67fc20d2f765e90b3517ac3
Chieng_john <chiengbulgai35@gmail.com>
2025-12-30 10:27:39 -0500

    Add files via upload

---
+++ Simple Blog App/scripts.js
@@ -0,0 +1,26 @@
+// const navigationMenu = document.getElementById('nav');
+// const storyContainer = document.getElementById('story1');
+// const projects = document.getElementById('project-list');
+// const contactForm = document.getElementById('contact-form');
+
+
+// navigationMenu.addEventListener('click', (Event)=>{
+//     const target = Event.target;
+//     if(target.tagName === "storyContainer"){
+//         storyContainer.scrollIntoView({behavior: "smooth"});
+//     } else if(target.tagName === "projects"){
+//         projects.scrollIntoView({behavior: "smooth"});
+//     } else if (target.tagName === "contactForm"){
+//         contactForm.scrollIntoView({behavior: "smooth"});
+//     }
+// })
+
+// // smooth scrolling for navigation links
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth'});
+
+//     })
+// })
+
